d8acf70881d00f7584f39c626f22b1f5ac431ee4
code-shoot <franksmithattheshoot@gmail.com>
2020-11-08 23:40:55 +0530

    Added Grid style

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 let search = document.getElementById("search");
 let results_container = document.querySelector(".results-container");
+let found_or_not = document.querySelector(".found-or-not");
 
 let show_text = (check, element) => {
     let result_for = document.createElement(`${element}`);
     result_for.innerHTML = (check) ? `Search results for '${input_value}':` : `There are no search results. Try again!`;
-    results_container.appendChild(result_for);
+    found_or_not.appendChild(result_for);
 }
 
 let createGrid = (meals) => {
-    let grid = document.createElement("")
+    meals.forEach( element => {
+        let img_container = document.createElement("div");
+        let image = document.createElement("img");
+
+        img_container.classList.add = "img_container";
+        image.src = element.strMealThumb;
+        image.appendChild(image);
+        results_container.appendChild(image);
+    });
 }
 
 let find = (e) => {
     if(e.keyCode === 13 && search.value.length != 0){
         input_value = search.value;
-
+        search.value = '';
         fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input_value}`)
         .then( response => response.json() )
         .then( result => result.meals)
@@ -34,4 +43,5 @@ let find = (e) => {
     }
  
 }
+
 search.addEventListener('keypress', find);
