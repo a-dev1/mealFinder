acc6d5a5571c35f9b65bf810addd2053eae9d41d
code-shoot <franksmithattheshoot@gmail.com>
2020-11-09 20:07:47 +0530

    added hover effect

--- script.js
+++ script.js
@@ -2,21 +2,24 @@ let search = document.getElementById("search");
 let results_container = document.querySelector(".results-container");
 let found_or_not = document.querySelector(".found-or-not");
 
-let show_text = (check, element) => {
-    let result_for = document.createElement(`${element}`);
-    result_for.innerHTML = (check) ? `Search results for '${input_value}':` : `There are no search results. Try again!`;
-    found_or_not.appendChild(result_for);
+let show_text = (check) => {
+    found_or_not.innerHTML = (check) ? `Search results for '${input_value}':` : `There are no search results. Try again!`;
 }
 
 let createGrid = (meals) => {
     meals.forEach( element => {
         let img_container = document.createElement("div");
         let image = document.createElement("img");
+        let meal_info = document.createElement("div");
 
-        img_container.classList.add = "img_container";
+        img_container.classList.add("img_container");
+        meal_info.classList.add("meal_info");
+
+        meal_info.innerText = element.strMeal;
         image.src = element.strMealThumb;
-        image.appendChild(image);
-        results_container.appendChild(image);
+        img_container.appendChild(image);
+        img_container.appendChild(meal_info);
+        results_container.appendChild(img_container);
     });
 }
 
@@ -29,12 +32,12 @@ let find = (e) => {
         .then( result => result.meals)
         .then( meals => {
             if(meals != null){
-                show_text(true, 'h2');
+                show_text(true);
                 createGrid(meals);
                 console.log(meals);
             }
             else {
-                show_text(false, 'p');
+                show_text(false);
             }
         })
  
