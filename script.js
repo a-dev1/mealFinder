ab3367d871e6e9e4c0ae17829c524527c71e35cc
code-shoot <franksmithattheshoot@gmail.com>
2020-11-10 10:06:25 +0530

    added infromation

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 let search = document.getElementById("search");
 let results_container = document.querySelector(".results-container");
 let found_or_not = document.querySelector(".found-or-not");
+let details_container = document.querySelector(".detail-container");
 
 let show_text = (check) => {
     found_or_not.innerHTML = (check) ? `Search results for '${input_value}':` : `There are no search results. Try again!`;
+}
+
+let showDetails = (element) => {
+    details_container.innerHTML = "";
+    let heading = document.createElement("h1");
+    let img = document.createElement("img");
+    let category = document.createElement("h3");
+    let region = document.createElement("h3");
+    let rc_container = document.createElement("div");
+    let instructions = document.createElement("p");
+    let ingredients_head = document.createElement("h2");
+    let ingredients_container = document.createElement("p"); 
+
+    heading.innerText = element.strMeal;
+    heading.classList.add("heading");
+    img.src = element.strMealThumb;
+    category.innerText = element.strCategory;
+    region.innerText = element.strArea;
+    rc_container.appendChild(category);
+    rc_container.appendChild(region);
+    instructions.innerText = element.strInstructions;
+    ingredients_head.innerText = "Ingredients";
+
+    details_container.appendChild(heading);
+    details_container.appendChild(img);
+    details_container.appendChild(rc_container);
+    details_container.appendChild(instructions);
+    details_container.appendChild(ingredients_head);
+
+    // for(let i=1; i<=20; i++){
+    //     if(strIngredient)
+    // }
 }
 
 let createGrid = (meals) => {
@@ -20,6 +53,9 @@ let createGrid = (meals) => {
         image.src = element.strMealThumb;
         img_container.appendChild(image);
         img_container.appendChild(meal_info);
+        img_container.addEventListener('click', () => {
+            showDetails(element);
+        })
         results_container.appendChild(img_container);
     });
 }
