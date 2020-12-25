7ac60a5bfa9eac720539b23d548746772abc3a01
code-shoot <franksmithattheshoot@gmail.com>
2020-12-25 15:44:01 +0530

    Optimizing app

--- script.js
+++ script.js
@@ -1,39 +1,22 @@
 let search = document.getElementById("search");
-let results_container = document.querySelector(".results-container");
 let found_or_not = document.querySelector(".found-or-not");
+let results_container = document.querySelector(".results-container");
 let details_container = document.querySelector(".detail-container");
 
-let show_text = (check) => {
-    found_or_not.innerHTML = (check) ? `Search results for '${input_value}':` : `There are no search results. Try again!`;
-}
-
 let showDetails = (element) => {
-    details_container.innerHTML = "";
-    let heading = document.createElement("h1");
-    let img = document.createElement("img");
-    let category = document.createElement("h3");
-    let region = document.createElement("h3");
-    let rc_container = document.createElement("div");
-    let instructions = document.createElement("p");
-    let ingredients_head = document.createElement("h2");
+    details_container.innerHTML = `
+        <h1 class="heading">${element.strMeal}</h1>
+        <img src="${element.strMealThumb}" alt="FoodImage"/>
+        <div class="rc-container">
+            <h3>${element.strCategory}</h3>
+            <h3>${element.strArea}</h3>
+        </div>
+        <p>${element.strInstructions}</p>
+        <h2>Ingredients</h2>
+        <div></div>
+    `;
+
     let ingredients_container = document.createElement("div"); 
-
-    heading.innerText = element.strMeal;
-    heading.classList.add("heading");
-    img.src = element.strMealThumb;
-    category.innerText = element.strCategory;
-    region.innerText = element.strArea;
-    rc_container.appendChild(category);
-    rc_container.appendChild(region);
-    rc_container.classList.add("rc-container");
-    instructions.innerText = element.strInstructions;
-    ingredients_head.innerText = "Ingredients";
-
-    details_container.appendChild(heading);
-    details_container.appendChild(img);
-    details_container.appendChild(rc_container);
-    details_container.appendChild(instructions);
-    details_container.appendChild(ingredients_head);
     ingredients_container.classList.add("ingredients-container");
 
     for(let i=1; i<=20; i++){
@@ -46,27 +29,38 @@ let showDetails = (element) => {
     }
 }
 
+//creating overview grids 
 let createGrid = (meals) => {
     results_container.innerHTML = "";
-    meals.forEach( element => {
+
+     meals.forEach( meal => {
         let img_container = document.createElement("div");
-        let image = document.createElement("img");
-        let meal_info = document.createElement("div");
-
         img_container.classList.add("img_container");
-        meal_info.classList.add("meal_info");
 
-        meal_info.innerText = element.strMeal;
-        image.src = element.strMealThumb;
-        img_container.appendChild(image);
-        img_container.appendChild(meal_info);
+        img_container.innerHTML = `
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+            <div class="meal_info">${meal.strMeal}</div>
+        `;
+
         img_container.addEventListener('click', () => {
-            showDetails(element);
+            showDetails(meal);
+            scroll({
+                top: details_container.offsetTop,
+                left: 0,
+                behavior: 'smooth'
+            })
         })
         results_container.appendChild(img_container);
     });
+
 }
 
+//Result Header
+let search_result = (check) => {
+    found_or_not.innerHTML = (check) ? `Search results for '${input_value}':` : `There are no search results. Try again!`;
+}
+
+//fetch meals from API
 let find = (e) => {
     if(e.keyCode === 13 && search.value.length != 0){
         input_value = search.value;
@@ -76,19 +70,15 @@ let find = (e) => {
         .then( result => result.meals)
         .then( meals => {
             if(meals != null){
-                show_text(true);
+                search_result(true);
                 createGrid(meals);
                 console.log(meals);
             }
             else {
-                show_text(false);
+                search_result(false);
             }
         })
- 
-
-
     }
- 
 }
 
 search.addEventListener('keypress', find);
